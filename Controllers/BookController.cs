d1e80f0747710333fbe189d0c022476398abfd9d
kcaj21 <alex.kcaj@gmail.com>
2024-01-17 22:40:50 +0000

    first commit

---
+++ Controllers/BookController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using DotNetCRUD.Models;
+using DotNetCRUD.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetCRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public BookController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Book>>> GetAllBooks()
+        {
+            var books = await _context.Books.ToListAsync();
+
+            return  Ok(books);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBook(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book is null)
+                return BadRequest("Book not found");
+
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Book>>> AddBook(Book book)
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Books.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Book>>> UpdateBook(Book updatedBook)
+        {
+            var dbBook = await _context.Books.FindAsync(updatedBook.Id);
+            if (dbBook is null)
+                return BadRequest("Book not found");
+
+            dbBook.Title = updatedBook.Title;
+            dbBook.Author = updatedBook.Author;
+            dbBook.Genre = updatedBook.Genre;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Books.ToListAsync());
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<List<Book>>> DeleteBook(int id)
+        {
+            var dbBook = await _context.Books.FindAsync(id);
+            if (dbBook is null)
+                return BadRequest("Book not found");
+
+            _context.Books.Remove(dbBook);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Books.ToListAsync());
+        }
+    }
+}
---
+++ Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using DotNetCRUD.Models;
+using Microsoft.EntityFrameworkCore;
+namespace DotNetCRUD.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
